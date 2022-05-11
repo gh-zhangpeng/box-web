abeba2b174affcc94ab7dc6e6295bc418090639f
zhangpeng <zhangpeng01@zuoyebang.com>
2022-05-11 11:45:36 +0800

    登陆种cookies

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,25 +1,28 @@
 <script setup>
 import { computed } from '@vue/reactivity';
 import http from '../utils/http'
+import Cookies from 'js-cookie'
 import { ref } from 'vue'
+import { ElMessage } from 'element-plus';
 
 //账户
-const email = ref("")
+const email = ref("zhangpeng.0304@aliyun.com")
 //密码
-const password = ref("")
+const password = ref("123456")
 //控制登陆按钮状态
 const canClick = computed(() => email.value.length > 0 && password.value.length > 0)
 
 //登陆按钮的操作
 function login(params) {
-    console.log(import.meta.env)
     http.post('/account/login', {
         email: email.value,
         password: password.value
     }).then(function (response) {
-        console.log(response);
+        Cookies.set('token', response.data.token, { expires: new Date(response.data.expiresAt) })
+        ElMessage.success("登陆成功")
     }).catch(function (error) {
         console.log(error);
+        ElMessage.error("登陆失败")
     });
 }
 </script>
@@ -28,6 +31,6 @@ function login(params) {
     <div class="login">
         <el-input v-model="email" placeholder="请输入账户" clearable />
         <el-input v-model="password" placeholder="请输入密码" clearable />
-        <el-button type="primary" :disabled="!canClick" @click="login">Primary</el-button>
+        <el-button type="primary" :disabled="!canClick" @click="login">登陆</el-button>
     </div>
 </template>
--- src/utils/request.js
+++ src/utils/request.js
@@ -23,24 +23,23 @@ service.interceptors.request.use(function (config) {
 service.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
-    return response;
+    return response.data;
 }, function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    // if (error.response) {
-    //     // 请求成功发出且服务器也响应了状态码，但状态代码超出了 2xx 的范围
-    //     console.log(error.response.data);
-    //     console.log(error.response.status);
-    //     console.log(error.response.headers);
-    // } else if (error.request) {
-    //     // 请求已经成功发起，但没有收到响应
-    //     // `error.request` 在浏览器中是 XMLHttpRequest 的实例，
-    //     // 而在node.js中是 http.ClientRequest 的实例
-    //     console.log(error.request);
-    // } else {
-    //     // 发送请求时出了点问题
-    //     console.log('Error', error.message);
-    // }
+    // Any status codes that falls outside the range of 2xx cause this function to trigger. Do something with response error
+    // 状态代码超出了 2xx 的范围
+    if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+    } else if (error.request) {
+        // 请求已经成功发起，但没有收到响应
+        // `error.request` 在浏览器中是 XMLHttpRequest 的实例，
+        // 而在node.js中是 http.ClientRequest 的实例
+        console.log(error.request);
+    } else {
+        // 发送请求时出了点问题
+        console.log('Error', error.message);
+    }
     return Promise.reject(error);
 });
 export default service
